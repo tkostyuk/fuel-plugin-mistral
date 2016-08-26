624941dfe1102b0f82e408660f9c5a1a333a2822
Kostyantyn Kalynovskyi <kkalynovskyi@mirantis.com>
2016-08-26 10:28:35 +0000

    mistral_dashboard

---
+++ deployment_scripts/puppet/modules/fuel_plugin_mistral_tasks/mistral_dashboard.pp
@@ -0,0 +1,30 @@
+class fuel_plugin_mistral_tasks::mistral_dashboard {
+  include fuel_plugin_mistral_tasks
+  include horizon::params
+
+  package { 'python-pip':
+    ensure => installed,
+  }
+
+  package { 'mistral-dashboard':
+    ensure => '2.0.0',
+    provider => pip,
+    require  => Package['python-pip']
+  }
+
+  file { $fuel_plugin_mistral_tasks::mistral_horizon_ext_file:
+    ensure => file,
+    content => template('fuel_plugin_mistral_tasks/_50_mistral.py'),
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
+  service { $horizon::params::http_service:
+    ensure => running,
+  }
+
+  Package['mistral-dashboard'] ->
+    File[$fuel_plugin_mistral_tasks::mistral_horizon_ext_file] ~>
+      Service[$horizon::params::http_service]
+}
