dd68812856ea8b6d3896ae3d7acc9b98ce2c63ec
root <root@nailgun.test.domain.local>
2016-09-06 11:58:39 +0000

    Repo Puppet manifest fixed

--- deployment_scripts/puppet/modules/fuel_plugin_mistral_tasks/manifests/repo.pp
+++ deployment_scripts/puppet/modules/fuel_plugin_mistral_tasks/manifests/repo.pp
@@ -1,23 +1,28 @@
-include apt
+class fuel_plugin_mistral_tasks::repo {
 
-apt::key {'fuel-infra':
-  id     => '3E5CBCC6DF05CD6558A75DB1BCE5CC461FA22B08',
-  source => 'http://perestroika-repo-tst.infra.mirantis.net/mos-repos/ubuntu/9.0/archive-mos9.0.key',
+  notice('MODULAR: fuel_plugin_mistral_tasks/repo.pp')
+
+  include apt
   
+  apt::key {'fuel-infra':
+    id     => '3E5CBCC6DF05CD6558A75DB1BCE5CC461FA22B08',
+    source => 'http://perestroika-repo-tst.infra.mirantis.net/mos-repos/ubuntu/9.0/archive-mos9.0.key',
+    
+  }
+  
+  
+  apt::source {'mos-proposed':
+    location => 'http://perestroika-repo-tst.infra.mirantis.net/mos-repos/ubuntu/9.0',
+    release  => 'mos9.0-proposed',
+    repos    => 'main',
+  }
+  
+  apt::pin { 'mos-proposed':
+    priority        => 1050,
+    release         => 'mos9.0-proposed',
+    codename        => 'mos9.0',
+    originator      => 'Mirantis',
+    label           => 'mos9.0',
+  }
+
 }
-
-
-apt::source {'mos-proposed':
-  location => 'http://perestroika-repo-tst.infra.mirantis.net/mos-repos/ubuntu/9.0',
-  release  => 'mos9.0-proposed',
-  repos    => 'main',
-}
-
-apt::pin { 'mos-proposed':
-  priority        => 1050,
-  release         => 'mos9.0-proposed',
-  codename        => 'mos9.0',
-  originator      => 'Mirantis',
-  label           => 'mos9.0',
-}
-
